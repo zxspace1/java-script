c07ecb83089410e24affe8dc695c5ab8b9f24f3c
Jacek Kowalczyk <zxspace1@gmail.com>
2018-10-06 11:53:25 +0200

    4-funcion

---
+++ 1-java-script/js/4-function.js
@@ -0,0 +1,18 @@
+let number = 5;
+
+console.log(number);
+
+function showNumber(num) {
+//    let number = "w srodku " + 15;
+    console.log(number);
+    
+    if (2 + 2 == 4) {
+//        let number = "w środku if ()" + 50;
+        console.log(number);
+    }
+//    console.log(number);
+    
+    return num;
+}
+
+showNumber(10);
