a56ef6e3b0d7b90ad77f2298904adea4ea4e2e94
Jacek Kowalczyk <zxspace1@gmail.com>
2018-10-06 12:35:36 +0200

    typy

---
+++ 1-java-script/js/5-datatypes.js
@@ -0,0 +1,25 @@
+let liczba = 50;
+
+let tekst = "Tu bedzie tekst-string";
+
+let truFalse = true;
+
+let tablica = [1,2,3].length; //tablica[0] --> 1
+
+let object = {klucz: "Monika"}; //object["klucz] lub object.klucz
+
+//console.log(typeof tablica);
+
+// ES5
+
+let number = 10;
+
+let zdanie = "Wyświetl" + number + "zakończ działanie";
+
+//console.log(działanie);
+
+//ES6
+let number2 = 10;
+
+let zdanie2 = `Wyświetl ${number2} i zakończ działanie`;
+
