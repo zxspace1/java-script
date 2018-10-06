e31a20545f04026fa56e9c56c18a5fd7ba19bdf2
Jacek Kowalczyk <zxspace1@gmail.com>
2018-10-06 14:06:13 +0200

    tablice

--- 1-java-script/js/6-arrays.js
+++ 1-java-script/js/6-arrays.js
@@ -10,13 +10,18 @@ tablica.unshift("Adam"); // dodaje na początku
 tablica.shift(); //usuwa elem na początku
 tablica.pop(); // usuwa element na końcu
 
-/*1 - znajdź element pod indexem 1
-0 - nic nie usuwaj;
-"Przemek, Lorem" dodaj "Przemek, Lorem"*/
-tablica.splice(1, 0, "Przemek", "Lorem");
-/*1 - znajdź element pod indexem 1
-2 - usuń 2 elementy;
-"Przemek, Lorem" dodaj "Przemek, Lorem"*/
-tablica.splice(1, 2, "Przemek", "Lorem" )
+///*1 - znajdź element pod indexem 1
+//0 - nic nie usuwaj;
+//"Przemek, Lorem" dodaj "Przemek, Lorem"*/
+//tablica.splice(1, 0, "Przemek", "Lorem");
+///*1 - znajdź element pod indexem 1
+//2 - usuń 2 elementy nic nie usuwaj;
+//"Przemek, Lorem" dodaj "Przemek, Lorem"*/
+//tablica.splice(1, 2, "Przemek", "Lorem" )
+
+tablica.reverse();
+
 
 console.log(tablica);
+//console.log(tablica.indexOf("Karolina"));
+
