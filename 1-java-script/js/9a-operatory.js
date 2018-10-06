ce356f2354eb0ac6fdd58fb8d40475414bd025a1
Jacek Kowalczyk <zxspace1@gmail.com>
2018-10-06 15:57:09 +0200

    oper

---
+++ 1-java-script/js/9a-operatory.js
@@ -0,0 +1,10 @@
+//let wartosc = (2 + 2 > 4-1);
+
+//wartosc = !(10 + 10 < 100);
+
+wartosc = 50 === "50"
+wartosc = (2 + 2 === 4 && 3 + 1 === 4);
+wartosc = (2 + 2 === 4 || 3 + 1 === 4);
+wartosc = (2 + 2 === 4) ? "Równa się 4" : "Nie równa się 4";
+
+console.log(wartosc);
