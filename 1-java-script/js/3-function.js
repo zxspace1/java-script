d596b582c8e54ed9b7c6e2d44465f487e75f1422
Jacek Kowalczyk <zxspace1@gmail.com>
2018-10-06 11:02:45 +0200

    init

---
+++ 1-java-script/js/3-function.js
@@ -0,0 +1,25 @@
+/*
+function wyswietlImie (imie) {
+//    return imie;
+    console.log(imie);
+}
+
+let wyswietlImie = function (imie) {
+    console.log(imie);
+}
+//console.log(wyswietlImie("Tomek"));
+let wynikWyswietlImie = wyswietlImie("Krystian");
+
+console.log(wynikWyswietlImie);
+
+//wyswietlImie("Tomek");
+*/
+
+function dodaj(jeden, dwa, trzy) {
+    let result = jeden + dwa + trzy;
+    return result;
+}
+
+//let sum = dodaj(1, 2, 3);
+//console.log(sum);
+console.log(dodaj(1, 2, 3))
