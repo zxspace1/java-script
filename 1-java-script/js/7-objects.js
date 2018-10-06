317009e8596c1ed24f16ec7ba8c56a28d1d43a3a
Jacek Kowalczyk <zxspace1@gmail.com>
2018-10-06 15:05:46 +0200

    osoba

--- 1-java-script/js/7-objects.js
+++ 1-java-script/js/7-objects.js
@@ -5,4 +5,31 @@ let adam = {
     nazwisko: 'Dou'
 };
 
-console.log(adam.nazwisko, adam.włosy);
+//console.log(adam.nazwisko, adam.włosy);
+
+
+class Osoba {
+    constructor (imie, nazwisko,wiek, kolorOczu) {
+        this.name = imie;
+        this.lastName = nazwisko;
+        this.age = wiek;
+        this.eyeColor = kolorOczu;
+    }
+    printInfo() {
+        let info = `Name: ${this.name}, last name: ${this.lastName}, wiek: ${this.age}`;
+            console.log(info);
+        
+    }
+}
+
+let mateusz = new Osoba("Mateusz", "Górski");
+//mateusz.age = 30;
+//mateusz.kolorOczu = "zielony";
+//
+//mateusz.kolorOczu = null;
+
+let lukasz = new Osoba("Łukasz", "Nowak", 50, "piwny");
+
+console.log(lukasz.age, lukasz.eyeColor);
+
+lukasz.printInfo();
