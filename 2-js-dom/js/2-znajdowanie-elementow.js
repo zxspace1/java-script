1871ad91fab1c202501bae6913015e4257a2a9d5
Jacek Kowalczyk <zxspace1@gmail.com>
2018-10-07 11:04:09 +0200

    znajdowanie

---
+++ 2-js-dom/js/2-znajdowanie-elementow.js
@@ -0,0 +1,17 @@
+let parFirst = document.getElementById("parFirst");
+
+console.log(parFirst);
+
+let link = document.getElementsByClassName("link");
+console.log(link);
+
+let paragraf = document.getElementsByTagName("p");
+//console.log(paragraf);
+
+console.log(paragraf[0]);
+
+let onlyLink = document.querySelector(".link")
+console.log(link);
+
+let allLinks = document.querySelectorAll(".link")
+console.log(allLinks);
