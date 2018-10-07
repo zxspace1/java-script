997203e64f0c5a26409cb9048fa3dd54ff222ce8
Jacek Kowalczyk <zxspace1@gmail.com>
2018-10-07 10:15:12 +0200

    json

---
+++ 1-java-script/js/11-json.js
@@ -0,0 +1,24 @@
+{
+    let jsonObject = {
+        'employees': [
+
+            {
+                'firstName': 'John',
+                'lastName': 'Doe',
+                'zainteresowania': ["sport", "programowanie"]
+            },
+            {
+                'firstName': 'Anna',
+                'lastName': 'Smith'
+            },
+            {
+                'firstName': 'Peter',
+                'lastName': 'Jones'
+            }
+    ]
+    }
+
+//    let pierwszaOsoba = `${jsonObject.employees[0].firstName} ${jsonObject.employees[0].lastName} interesuje się ${jsonObject.employees[0].zainteresowania}`;
+    let pierwszaOsoba2 = `${jsonObject.employees[0].firstName}`;
+
+//    console.log(jsonObject);
